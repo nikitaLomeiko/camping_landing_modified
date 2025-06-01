5cc94956af64438bf8e3bbac31801089c3af580c
Nikita <nikitalomieiko@gmail.com>
2025-06-01 10:29:47 +0300

    init

---
+++ src/vue/content/sections/HeroSection.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Heading Section -->
+    <PageHeader :id="props.id"
+                title="*Foxy* Landing Page"
+                subtitle="An agency template by <strong>Ryan Balieiro</strong> – made with <strong>Vue3</strong> and <strong>Bootstrap 5</strong>"
+                logo-url="images/logo/agency-logo.png"
+                :show-button="true"
+                button-icon="fa-solid fa-search"
+                button-label="Find Out More"
+                button-url="#about"/>
+</template>
+
+<script setup>
+import PageHeader from "/src/vue/components/layout/PageHeader.vue"
+
+const props = defineProps({
+    id: String
+})
+</script>
+
+<style lang="scss" scoped>
+@import "/src/scss/_theming.scss";
+</style>
